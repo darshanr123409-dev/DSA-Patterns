3885707a422f0a96e968115ec145fae766bd2271
darshanr123409-dev <darshanr123409@gmail.com>
2026-09-19 21:45:20 +0530

    Added Fast and slow -Approach  problems

---
+++ Fast & Slow pointers/Find the Duplicate Number.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int fast =0;
+        int slow =0;
+
+        while(true ){
+            slow = nums[slow];
+            fast = nums[fast];
+            fast = nums[fast];
+            if(slow == fast){
+                break;
+            }
+        }
+            slow =0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow ;
+
+    }
+};

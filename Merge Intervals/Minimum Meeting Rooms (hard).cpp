64a79321050496054368088ca926eb2c4c8e1743
darshanr123409-dev <darshanr123409@gmail.com>
2026-09-25 23:05:33 +0530

    Added merge intervals Approach problems

---
+++ Merge Intervals/Minimum Meeting Rooms (hard).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int minMeetingRooms(vector<int> &start, vector<int> &end) {
+        // code here
+        int n = start.size();
+        int m = end.size();
+        sort(start.begin(),start.end());
+        sort(end.begin(),end.end());
+        int res=0;
+        int room =0;
+        int i=0,j=0;
+        
+        while( i<n && j<m){
+            if(start[i] < end[j] ){
+                room++;
+                res=max(res,room);
+                i++;
+            }
+            else {
+                room--;
+                j++;
+            }
+        }
+        return res;
+    }
+};
